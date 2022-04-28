df1f935e4e21a21764833b077957cfffa37cafc9
junerver <junerver@gmail.com>
2022-04-28 12:35:25 +0800

    feat：添加useMount&useDebounce两个hook

--- src/screens/project-list/index.js
+++ src/screens/project-list/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import List from "./list";
 import SearchPanel from "./search-panel";
-import { cleanObject, isFalsy } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 import qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -11,27 +11,28 @@ const ProjectListScreen = () => {
     name: "",
     personId: "",
   });
+  const debounceParam = useDebounce(param, 2000);
   //下拉列表的状态
   const [users, setUsers] = useState([]);
   //查询结果的状态
   const [list, setList] = useState([]);
   //输入变化后请求接口
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
       }
-    );
-  }, [param]);
-  useEffect(() => {
+    });
+  }, [debounceParam]);
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, []);
+  });
   return (
     <div>
       <SearchPanel param={param} setParam={setParam} users={users} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 //在js中函数传入对象是一个不好的方式，因为函数可能会污染对象
@@ -13,4 +14,42 @@ export const cleanObject = (obj) => {
   });
   //返回值是一个新对象
   return result;
+};
+
+//自定义的仅在初始化时加载一次的钩子
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+//去抖函数，传入一个原本要执行的函数，并设定去抖时长，然后返回去抖后的函数
+// const debounce = (func, delay) => {
+//   let timer;
+//   return () => {
+//     if (timer) {
+//       clearTimeout(timer);
+//     }
+//     timer = setTimeout(() => {
+//       func();
+//     }, delay);
+//   };
+// };
+// const log = debounce(() => console.log("log"), 5000);
+// log();
+// log();
+// log();
+
+//将一个抖动的state传入，返回一个去抖的state
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+  return debouncedValue;
 };
