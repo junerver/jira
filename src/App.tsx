607396e92cc0d89663ae54840b88fa5dfd49b8e7
junerver <junerver@gmail.com>
2022-05-16 16:15:24 +0800

    配置prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import ErrorBoundary from 'components/error-boundary';
-import { FullPageError } from 'components/lib';
-import { useAuth } from 'context/auth-context';
-import './App.css';
-import React, { Suspense } from "react";
-import { Spin } from "antd";
+import ErrorBoundary from 'components/error-boundary'
+import { FullPageError } from 'components/lib'
+import { useAuth } from 'context/auth-context'
+import './App.css'
+import React, { Suspense } from 'react'
+import { Spin } from 'antd'
 
 //懒加载AuthorizedApp，注意懒加载必须是default导出
-const LazyAuthorizedApp = React.lazy(() => import('authorized-app'));
-const LazyUnauthorizedApp = React.lazy(() => import('unauthorized-app'));
+const LazyAuthorizedApp = React.lazy(() => import('authorized-app'))
+const LazyUnauthorizedApp = React.lazy(() => import('unauthorized-app'))
 
 function App() {
-    const { user } = useAuth()
-    return (
-        <div className='App'>
-            <ErrorBoundary fallbackRender={FullPageError}>
-                <Suspense fallback={<Spin size={"large"} />}>
-                    {user ? <LazyAuthorizedApp /> : <LazyUnauthorizedApp />}
-                </Suspense>
-            </ErrorBoundary>
-        </div>
-    );
+  const { user } = useAuth()
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageError}>
+        <Suspense fallback={<Spin size={'large'} />}>
+          {user ? <LazyAuthorizedApp /> : <LazyUnauthorizedApp />}
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/authorized-app.tsx
+++ src/authorized-app.tsx
@@ -1,16 +1,16 @@
-import styled from "@emotion/styled";
-import { Row } from "components/lib";
-import { useAuth } from "context/auth-context";
-import ProjectListScreen from "screens/project-list";
-import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
-import { Button, Dropdown, Menu } from "antd";
-import { Navigate, Route, Routes } from "react-router";
-import { ProjectScreen } from "screens/project";
+import styled from '@emotion/styled'
+import { Row } from 'components/lib'
+import { useAuth } from 'context/auth-context'
+import ProjectListScreen from 'screens/project-list'
+import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
+import { Button, Dropdown, Menu } from 'antd'
+import { Navigate, Route, Routes } from 'react-router'
+import { ProjectScreen } from 'screens/project'
 
-import { resetRoute } from "utils";
-import { ProjectModal } from "screens/project-list/project-modal";
-import { ProjectPopover } from "components/project-popover";
-import { UserPopover } from "./components/user-popover";
+import { resetRoute } from 'utils'
+import { ProjectModal } from 'screens/project-list/project-modal'
+import { ProjectPopover } from 'components/project-popover'
+import { UserPopover } from 'components/user-popover'
 
 const AuthorizedApp = () => {
   //控制模态的显示还是隐藏
@@ -27,10 +27,10 @@ const AuthorizedApp = () => {
       </Main>
       <ProjectModal />
     </Container>
-  );
-};
+  )
+}
 
-export default AuthorizedApp;
+export default AuthorizedApp
 
 //页头
 const PageHeader = () => {
@@ -38,11 +38,11 @@ const PageHeader = () => {
     <Header between={true}>
       <HeaderLeft gap={true}>
         <Button
-          style={{ display: "flex", alignItems: "center" }}
+          style={{ display: 'flex', alignItems: 'center' }}
           type="link"
           onClick={resetRoute}
         >
-          <SoftwareLogo width={"18rem"} color={"rgb(38,132,255)"} />
+          <SoftwareLogo width={'18rem'} color={'rgb(38,132,255)'} />
         </Button>
         <ProjectPopover />
         <UserPopover />
@@ -51,20 +51,20 @@ const PageHeader = () => {
         <User />
       </HeaderRight>
     </Header>
-  );
-};
+  )
+}
 
 //页头右侧用户
 const User = () => {
-  const { logout, user } = useAuth();
+  const { logout, user } = useAuth()
   return (
     <Dropdown
       overlay={
         <Menu
           items={[
             {
-              key: "logout",
-              label: "退出",
+              key: 'logout',
+              label: '退出',
               onClick: logout,
             },
           ]}
@@ -75,24 +75,24 @@ const User = () => {
         Hi,{user?.name}
       </Button>
     </Dropdown>
-  );
-};
+  )
+}
 
 //使用grid作为布局系统
 const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem 1fr;
   height: 100vh;
-`;
+`
 
 //grid-area 用于为grid区域起名
 const Header = styled(Row)`
   padding: 3.2rem;
   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-`;
-const HeaderLeft = styled(Row)``;
-const HeaderRight = styled.div``;
+`
+const HeaderLeft = styled(Row)``
+const HeaderRight = styled.div``
 const Main = styled.main`
   display: flex;
   overflow: hidden;
-`;
+`
