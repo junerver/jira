5ebd471c7125b3ecf0960a37ff925c82a4d06f69
junerver <junerver@gmail.com>
2022-06-09 08:09:22 +0800

    格式化代码

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -1,47 +1,51 @@
-import { useMemo } from "react"
-import { useProject } from "utils/project"
-import { useSetUrlSearchParam, useUrlQueryParam } from "utils/url"
+import { useMemo } from 'react'
+import { useProject } from 'utils/project'
+import { useSetUrlSearchParam, useUrlQueryParam } from 'utils/url'
 
 export const useProjectsSearchParams = () => {
-    const [param, setParam] = useUrlQueryParam(['name', 'personId'])
-    return [
-        useMemo(
-            () => ({ ...param, personId: Number(param.personId) || undefined }),
-            [param]),
-        setParam
-    ] as const
+  const [param, setParam] = useUrlQueryParam(['name', 'personId'])
+  return [
+    useMemo(
+      () => ({ ...param, personId: Number(param.personId) || undefined }),
+      [param]
+    ),
+    setParam,
+  ] as const
 }
 
 export const useProjectsQueryKey = () => {
-    const [searchParams] = useProjectsSearchParams()
-    return ['projects', searchParams]
+  const [searchParams] = useProjectsSearchParams()
+  return ['projects', searchParams]
 }
 
 //用于编辑项目的自定义钩子，可以打开模态窗口编辑项目内容
 export const useProjectModal = () => {
-    const [{ projectCreate }, setProjectModalOpen] = useUrlQueryParam([
-        'projectCreate'
-    ])
-    const [{ editingProjectId }, setEditingProjectId] = useUrlQueryParam([
-        'editingProjectId'
-    ])
+  const [{ projectCreate }, setProjectModalOpen] = useUrlQueryParam([
+    'projectCreate',
+  ])
+  const [{ editingProjectId }, setEditingProjectId] = useUrlQueryParam([
+    'editingProjectId',
+  ])
 
-    const setUrlParams = useSetUrlSearchParam()
+  const setUrlParams = useSetUrlSearchParam()
 
-    //获取要编辑的页面
-    const { data: editingProject, isLoading } = useProject(Number(editingProjectId))
+  //获取要编辑的页面
+  const { data: editingProject, isLoading } = useProject(
+    Number(editingProjectId)
+  )
 
-    const open = () => setProjectModalOpen({ projectCreate: true })
-    //关闭窗口时，清除编辑项目的id
-    const close = () => setUrlParams({ projectCreate: '', editingProjectId: '' })
-    const startEdit = (id: number) => setEditingProjectId({ editingProjectId: id })
+  const open = () => setProjectModalOpen({ projectCreate: true })
+  //关闭窗口时，清除编辑项目的id
+  const close = () => setUrlParams({ projectCreate: '', editingProjectId: '' })
+  const startEdit = (id: number) =>
+    setEditingProjectId({ editingProjectId: id })
 
-    return {
-        projectModalOpen: projectCreate === 'true' || Boolean(editingProjectId),
-        open,
-        close,
-        startEdit,
-        editingProject,
-        isLoading
-    }
-};
+  return {
+    projectModalOpen: projectCreate === 'true' || Boolean(editingProjectId),
+    open,
+    close,
+    startEdit,
+    editingProject,
+    isLoading,
+  }
+}
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,29 +1,32 @@
-import { useMemo, useState } from "react"
-import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
-import { cleanObject, isNullOrUndefined, isVoid, subset } from "utils"
+import { useMemo, useState } from 'react'
+import { URLSearchParamsInit, useSearchParams } from 'react-router-dom'
+import { cleanObject, subset } from 'utils'
 
 //需要注意传入的必须是一个数组对象，返回值是一个对象，使用reduce函数非常的优雅
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-    const [searchParams] = useSearchParams()
-    const setSearchParams = useSetUrlSearchParam()
-    const [stateKeys] = useState(keys)
-    return [
-        useMemo(
-            () => subset(Object.fromEntries(searchParams), stateKeys) as { [key in K]: string },
-            [searchParams, stateKeys]
-        ),
-        (params: Partial<{ [key in K]: unknown }>) => setSearchParams(params)
-    ] as const
+  const [searchParams] = useSearchParams()
+  const setSearchParams = useSetUrlSearchParam()
+  const [stateKeys] = useState(keys)
+  return [
+    useMemo(
+      () =>
+        subset(Object.fromEntries(searchParams), stateKeys) as {
+          [key in K]: string
+        },
+      [searchParams, stateKeys]
+    ),
+    (params: Partial<{ [key in K]: unknown }>) => setSearchParams(params),
+  ] as const
 }
 
 //专门用来修改url的查询参数的钩子
 export const useSetUrlSearchParam = () => {
-    const [searchParam, setSearchParam] = useSearchParams()
-    return (params: Partial<{ [key in string]: unknown }>) => {
-        const o = cleanObject({
-            ...Object.fromEntries(searchParam),
-            ...params
-        }) as URLSearchParamsInit
-        return setSearchParam(o)
-    }
-};
+  const [searchParam, setSearchParam] = useSearchParams()
+  return (params: Partial<{ [key in string]: unknown }>) => {
+    const o = cleanObject({
+      ...Object.fromEntries(searchParam),
+      ...params,
+    }) as URLSearchParamsInit
+    return setSearchParam(o)
+  }
+}
