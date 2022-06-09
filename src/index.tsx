74b4e1eb8e1bfabc41379a92525433b0bb613942
junerver <junerver@gmail.com>
2022-06-09 15:33:13 +0800

    格式化代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { loadServer, DevTools } from 'jira-dev-tool';
-import "antd/dist/antd.less";
-import { AppProviders } from './context';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { DevTools, loadServer } from 'jira-dev-tool'
+import 'antd/dist/antd.less'
+import { AppProviders } from 'context'
 
 loadServer(() =>
-  ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  ).render(
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <AppProviders>
         <DevTools />
         <App />
       </AppProviders>
     </React.StrictMode>
-  ));
+  )
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
